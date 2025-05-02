9461363eb8c710cc20ec64b74bcea650d6789507
nahusenayElias <eliasars@yahoo.com>
2025-05-03 01:43:54 +0300

    feat: Card added

--- src/components/StoreNavBar.tsx
+++ src/components/StoreNavBar.tsx
@@ -4,17 +4,13 @@ const StoreNavBar = () => {
   return (
     <Navbar bg="primary" variant="dark">
         <Container>
-            <Navbar.Brand href="/">Online store
-
-
-            </Navbar.Brand>
+            <Navbar.Brand href="/">Online store</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/cart">Cart</Nav.Link>
             </Nav>
         </Container>
-
     </Navbar>
   )
 }
-export default StoreNavBar
+export default StoreNavBar;
