73756f20cc5824477a5864dc64a9c80c71c0982f
nahusenayElias <eliasars@yahoo.com>
2025-05-03 01:42:11 +0300

    feat: Implement roaster notification

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,28 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import Product from "./Product";
 import { fetchProducts } from "../features/productSlice";
 import { calculateTotalOfProducts } from "../utils/utils";
+import { Alert, Container, Row } from "react-bootstrap";
 
 const List = () => {
-
   const products = useAppSelector((state) => state.products.products);
   const dispatch = useAppDispatch();
+  const [notification, setNotification] = useState<string | null>(null);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => setNotification(null), 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
+
+  const totalOfProducts = calculateTotalOfProducts(products);
+
   return (
-    <div>
-      <h1>List will be here</h1>
-      <h2>{calculateTotalOfProducts(products)}</h2>
-
-    {products.map((product) => (
-      <Product {...product} />
-
-      ))}
+    <Container className="my-4">
+      {/* Notification Toast */}
+      <div style={{ position: 'fixed', top: '20px', right: '20px', zIndex: 1000 }}>
+        {notification && (
+          <Alert variant="success" onClose={() => setNotification(null)} dismissible>
+            {notification}
+          </Alert>
+        )}
       </div>
-  )
-}
+
+      <h2>Total in Cart: ${totalOfProducts}</h2>
+      <Row className="g-4">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            {...product}
+            
+          />
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
 export default List;
